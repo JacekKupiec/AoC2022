f08789d97e4cfe6548cd9f2c97934ad86bbb78f4
Jacek Kupiec <jacek.kupiec@o2.pl>
2022-12-07 19:57:49 +0100

    day 6 finished

---
+++ day_06/src/main.rs
@@ -0,0 +1,26 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const MARKER_LENGTH: usize = 14;
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let mut reader = BufReader::new(file);
+    let mut buffer = String::new();
+
+    if let Err(message) = reader.read_line(&mut buffer) {
+        eprintln!("The error occured. Can't process: {}", message);
+        return;
+    }
+
+    let position = buffer.trim_end().as_bytes().windows(MARKER_LENGTH).position(|window| {
+        let mut set = HashSet::with_capacity(14);
+        let mut range = 0..14;
+
+        return range.all(|idx| set.insert(window[idx]));
+    })
+    .unwrap();
+
+    println!("{}", position + MARKER_LENGTH);
+}
