ca2cbf65f8e96f01a976943bc4eb0e8091f86758
Jacek Kupiec <jacek.kupiec@o2.pl>
2024-04-14 21:17:02 +0200

    a little bit further

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use scanf::sscanf;
@@ -31,6 +32,22 @@ impl Blueprint {
             geode_robot_obsidian_cost
         }
     }
+}
+
+#[derive(Hash)]
+struct ResourcesInventory {
+    ore: i32,
+    clay: i32,
+    obsidian: i32,
+    geode: i32
+}
+
+#[derive(Hash)]
+struct ResourcesIncrements {
+    ore_increment: i32,
+    clay_increment: i32,
+    obsidian_increment: i32,
+    geode_increment: i32
 }
 
 fn main() {
@@ -79,9 +96,36 @@ fn main() {
 }
 
 fn equality_level(blueprint: &Blueprint) -> i32 {
-    blueprint.id * max_geodes_open(blueprint)
+    let mut resources_inventory = ResourcesInventory {
+        ore: 0,
+        clay: 0,
+        obsidian: 0,
+        geode: 0
+    };
+
+    let mut resources_increments = ResourcesIncrements {
+        ore_increment: 1,
+        clay_increment: 1,
+        obsidian_increment: 1,
+        geode_increment: 1
+    };
+
+    let mut cache = HashMap::new();
+
+    blueprint.id * max_geodes_open(blueprint, &mut cache, &mut resources_inventory, &mut resources_increments, 24)
 }
 
-fn max_geodes_open(blueprint: &Blueprint) -> i32 {
-    todo!()
+fn max_geodes_open(
+    blueprint: &Blueprint,
+    cache: &mut HashMap<(ResourcesInventory, ResourcesIncrements, i32), i32>,
+    resources: &mut ResourcesInventory,
+    resources_increments: &mut ResourcesIncrements, time: i32)
+    -> i32 {
+    if time <= 0 {
+        return 0;
+    }
+
+    if let Some(result) = cache.contains_key(&(resources, resources_increments, time)) {
+        return result;
+    }
 }
